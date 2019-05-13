a3d01ce10eb1a7da640a69b7804fd14c75d0ea6e
Jack Woods <jckwoods509@gmail.com>
2019-05-14 11:17:49 +1200

    Removed GetWeather2 code and tests as it is just a duplicate Testing modifying API Call to NZ zip codes

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -6,16 +6,16 @@ var REQUEST = require('request');
 var request = REQUEST.defaults( {
     strictSSL: false
 });
-
-var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
+// Units = metric for metric units
+var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric";
 
 exports.getWeather = function(req, res) {
 	var zip = req.query.zip;
 	if( (zip === null) || (typeof(zip) === 'undefined') ) {
 		return res.status(400).send('zip missing');
 	}
-
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+    // Must include country in URL as it defaults to US
+	var aurl = OPENWEATHERURL + '&zip=' + zip + ',nz';
 
 	request({
 		method: 'GET',
@@ -38,37 +38,5 @@ exports.getWeather = function(req, res) {
 
 };
 router.get('/getWeather', exports.getWeather);
-
-
-exports.getWeather2 = function(req, res) {
-	var zip = req.query.zip;
-	if( (zip === null) || (typeof(zip) === 'undefined') ) {
-		return res.status(400).send('zip missing');
-	}
-
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
-
-	request({
-		method: 'GET',
-        url: aurl,
-  		json: true
-    }, function(err, resp, body) {
-    	if(err) {
-    		res.status(400).send('Failed to get the data');
-    		//console.error("Failed to send request to openweathermap.org", err);
-    	} else {
-    		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
-    			var response = {city: body.name, weather: weath};
-    			return res.status(200).send(response);
-    		} else {
-                return res.status(400).send({msg:'Failed'});
-            }
-    	}
-    });
-
-};
-router.get('/getWeather2', exports.getWeather2);
-
 
 exports.router = router;
