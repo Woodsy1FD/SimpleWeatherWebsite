52ecceb7b2c530834051a2438c20805c790d97df
Jack Woods <jckwoods509@gmail.com>
2019-05-16 10:36:43 +1200

    Was calling an id that did not exist

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -69,7 +69,7 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
     };
 
     // Setup Google Maps
-    $scope.map = new google.maps.Map(document.getElementById(map), {
+    $scope.map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8
     });
