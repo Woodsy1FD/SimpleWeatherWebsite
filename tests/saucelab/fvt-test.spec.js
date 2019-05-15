b978c0fb47ae6e6143bf64b665c9cf030d585535
Jack Woods <jckwoods509@gmail.com>
2019-05-15 12:39:59 +1200

    Modified FVT test to test UI for new functionality

--- tests/saucelab/fvt-test.spec.js
+++ tests/saucelab/fvt-test.spec.js
@@ -47,8 +47,8 @@
       });
     });
 
-    it("Check for zip4 element", function(done) {
-      driver.isElementPresent(webdriver.By.id('zip4')).then(function(present) {
+    it("Check for city4 element", function(done) {
+      driver.isElementPresent(webdriver.By.id('city4')).then(function(present) {
         assert.equal(present, true);
         done();
       });
@@ -61,18 +61,18 @@
       });
     });
 
-    it("Enter zip code and check output - 1", function(done) {
-      driver.findElement(webdriver.By.id('zip1')).sendKeys('78613');
-      driver.findElement(webdriver.By.id('zip1city')).getText().then(function(text) {
-        assert.equal(text, ' Anderson Mill');
+    it("Enter city name and check output - 1", function(done) {
+      driver.findElement(webdriver.By.id('city1')).sendKeys('Hamilton');
+      driver.findElement(webdriver.By.id('city1city')).getText().then(function(text) {
+        assert.equal(text, ' Hamilton');
         done();
       });
     });
 
     it("Enter zip code and check output - 2", function(done) {
-      driver.findElement(webdriver.By.id('zip4')).sendKeys('75038');
-      driver.findElement(webdriver.By.id('zip4city')).getText().then(function(text) {
-        assert.equal(text, ' Irving');
+      driver.findElement(webdriver.By.id('city4')).sendKeys('Auckland');
+      driver.findElement(webdriver.By.id('city4city')).getText().then(function(text) {
+        assert.equal(text, ' Auckland');
         done();
       });
     });
